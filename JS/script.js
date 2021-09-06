6f025ef5935b7d638891ad05d6daf0b429612caa
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-09-06 07:38:49 +0300

    check if year is leap or not

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,13 @@
             var yourname = document.querySelector("form select[name=yourname]");
             var month = document.querySelector("form select[name=month]");
             var year = document.querySelector("form input[name=year]");
+
+
+
+  
+          // find the year if it is a leap year
+            function findleapyear(arrayvalues) {
+                return (arrayvalues % 4 == 0 && (arrayvalues % 100 != 0 || arrayvalues % 400 == 0));
+            }
+
+            
