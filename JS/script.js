7c4fca94519407e34b1f23dbbf63eb039f1c99ae
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-09-06 21:07:51 +0300

    Find the total number of days

--- JS/script.js
+++ JS/script.js
@@ -26,7 +26,32 @@
 
             month.addEventListener("input", finddaysnumber);
             year.addEventListener("input", finddaysnumber);
+
+            // gettng total number of days
+            function getNumberOfDaysMonth(arrayvalues, isleap = 0) {
+                return {
+                    "January": 31,
+                    "February": 28 + isleap,
+                    "March": 31,
+                    "April": 30,
+                    "May": 31,
+                    "June": 30,
+                    "July": 31,
+                    "August": 31,
+                    "September": 30,
+                    "October": 31,
+                    "November": 30,
+                    "December": 31
+                }[arrayvalues];
+            }
+
        
             function finddaysnumber() {
                 yourname.innerHTML = "<option selected disabled value='-'>Select yourname</option>" + (arraylist(getNumberOfDaysMonth(month.value, (findleapyear(year.value) ? 1 : 0))).map(el => "<option value='" + el + "'>" + el + "</option>").join(""));
+            }
+
+
+
+            function yournamedisp(arrayvalues) {
+                return document.getElementById(arrayvalues);
             }
