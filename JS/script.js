ece7a6d3748246b3288f8958b571a7b8db59dab9
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-09-06 07:36:18 +0300

    add the js declarations of input

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+
+
+            var mygender = document.querySelector("form select[name=mygender]");   
+             var male = document.getElementById("male").value;     
+             var female = document.getElementById("female").value;         
+            var yourname = document.querySelector("form select[name=yourname]");
+            var month = document.querySelector("form select[name=month]");
+            var year = document.querySelector("form input[name=year]");
